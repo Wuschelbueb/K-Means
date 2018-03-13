55a0fd3a641f8b8364114e889df7f8657d70c2bb
Wuschelbueb <Wuschelbueb@users.noreply.github.com>
2018-03-13 19:58:56 +0100

    smart read file

--- kmeans.py
+++ kmeans.py
@@ -1,18 +1,15 @@
 import numpy as np
 import csv
 
-def loadData(filename, dataSet = []):    
+def loadData(filename):    
     #split csv-rows up; array[0] = size 1; array[1] = size 784
     csv_array = np.genfromtxt(filename, delimiter=',', dtype=int)
-    #number to be at position: [0][x], pixelvalues at: [1][x]
-    split = np.split(csv_array,[1],axis=1)
-    dataSet.append(split)
-    print("hi")
+    #array[row, column]
+    samples = csv_array[:,1:]
+    labels = csv_array[:,0]
+    return samples, labels
 def main():
-    trainSet = []
-    loadData("simple_train.csv", trainSet)
-    test = np.array([trainSet[0][0], trainSet[0][1]])
-    test.append(trainSet[0][1])
+    train_samples, train_labels = loadData("simple_train.csv")
     print("hello")
 
 main()
